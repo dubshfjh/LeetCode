8270f712dca4a10b1f2cfb808cad189623e4d39b
dubshfjh <dubshfjhk@gmail.com>
2016-10-29 18:27:36 +0800

    检查二叉树是否equal：先序变量里，分析每层递归的p,q节点：如果p,q都为NULL or p,q都不为空而且p->val==q->val，则本层return true；否则本层return false

---
+++ 100.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p==NULL && q==NULL){
+            return true;
+        }
+        else if(p && q){
+            if(p->val == q->val){
+                return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+            }
+            else{
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+};
