2ca781edce1d2ad289ded1debe6c76a9ade25487
dubshfjh <dubshfjhk@gmail.com>
2016-11-13 19:46:12 +0800

    总利润如何最大：一旦有盈利空间就去买卖货物，也就是说把每一段价格递增区间的价格增长量加起来即可。核心思想：每一段单调递增区间的收益累加

--- 122.cpp
+++ 122.cpp
@@ -74,3 +74,27 @@ public:
         return res_profit;
     }
 };
+
+网上简化算法的核心思想：每一段单调递增区间的收益累加。
+class Solution {
+public:
+    int maxProfit(vector<int> &prices) {
+        int m = prices.size();
+        if(m == 0){
+            return 0;
+        }
+            
+        int ret = 0;
+        int buy = prices[0];
+        int last = prices[0];//last记录单调递增序列的最后一个元素
+        for(int i = 1; i < m; i ++){
+            if(prices[i] < last){
+                ret += (last - buy);
+                buy = prices[i];
+            }
+            last = prices[i];
+        }
+        ret += (last - buy);
+        return ret;
+    }
+};
